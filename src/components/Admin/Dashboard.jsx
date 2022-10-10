bb5f938474831c252c73c2e50e75084f24c18296
chiscocode <patrickchukwudifu716@gmail.com>
2022-10-10 19:56:38 +0100

    second

--- src/components/Admin/Dashboard.jsx
+++ src/components/Admin/Dashboard.jsx
@@ -34,16 +34,14 @@ render() {
         <header className="bg-nav">
             <div className="flex justify-between">
                 <div className="p-1 mx-3 inline-flex items-center">
-                    <i className="fas fa-bars pr-2 text-black" onClick={this.sidebarToggle}></i>
-                    <h1 className="text-black p-2">Logo</h1>
+                    <i className="fas fa-bars pr-2 text-white" onClick={this.sidebarToggle}></i>
+                    <h1 className="text-white p-2">Real estate</h1>
                 </div>
                 <div className="p-1 flex flex-row items-center">
                     <img  onClick={this.profileToggle} className="inline-block h-8 w-8 rounded-full" src="https://therichpost.com/wp-content/uploads/2021/03/avatar2.png" alt="" />
                     <a href="#" onClick={this.profileToggle}  className="text-white p-2 no-underline hidden md:block lg:block">Jassa The Rich</a>
                     <div id="ProfileDropDown" className="rounded hidden shadow-md bg-white absolute pin-t mt-12 mr-1 pin-r">
                         <ul className="list-reset">
-                          <li><a href="#" className="no-underline px-4 py-2 block text-black hover:bg-grey-light">My account</a></li>
-                          <li><a href="#" className="no-underline px-4 py-2 block text-black hover:bg-grey-light">Notifications</a></li>
                           <li><hr className="border-t mx-2 border-grey-ligght" /></li>
                           <li><button  onClick={this.logout} className="no-underline px-4 py-2 block text-black hover:bg-grey-light">Logout</button></li>
                         </ul>
